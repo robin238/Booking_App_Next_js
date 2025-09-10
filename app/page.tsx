9d59de063a38c87b92beeae873ab8ee1dc0f528c
robinina17 <robinina17@gmail.com>
2025-09-10 15:26:39 +0700

    feat: card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import Hero from "@/components/hero";
+import Main from "@/components/main";
+
 export default function Home() {
   return (
-    <div className="">
-      <h1>Jome page</h1>
+    <div>
+      <Hero />
+      <div className="mt-16">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold uppercase"> Room & Rates</h1>
+          <p className="py-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+            non.
+          </p>
+        </div>
+        <Main />
+      </div>
     </div>
   );
 }
