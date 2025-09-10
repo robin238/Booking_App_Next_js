c8e73d0e06cc1fede1182a64866465ff8fa2e5f2
robinina17 <robinina17@gmail.com>
2025-09-10 10:40:49 +0700

    feat: footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/navbar";
+import Footer from "@/components/footer";
 
 const raleway = Raleway({
   variable: "--font-raleway",
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={`${raleway.variable} antialiased`}>
         <Navbar />
         <main className="bg-gray-50 min-h-screen">{children}</main>
+        <Footer />
       </body>
     </html>
   );
